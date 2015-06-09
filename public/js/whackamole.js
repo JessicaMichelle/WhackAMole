c734d8747a5e4b5607a5f9abe8594283a65702bf
Jessica Michelle <JessicaMichelle@Jessicas-MacBook-Air.local>
2015-06-08 20:09:24 -0500

    Added functions to initiate random hole. Changed cursor icon. Added mole photo.

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -5,24 +5,29 @@ $(document).ready(function(){
 		var game = {
 			moles: $('.mole'),
 			score: 0,
-			level: 1,
-			timer: 5,
+			timer: 30,
 			interval: '',
 			
 			//Function to initiate the game start function. 
 			init: function(){
 				game.gameStart();
+				game.moles.click(function(){
+					game.hideMole();
+					game.addScore();
+				});
+
 			},
 
 			// Function to add event listener to start button and call 
 			// function to initiate interval and random hole choice.
 			gameStart: function(){
 				$(".start").click(function(){
-					game.showMole();
+					game.startInterval();
+					game.startTimer();
 				});
 			},
 
-			// select random hole using math.random.
+			// Select random hole using math.random.
 			getRandomHole: function(){
 				return Math.floor(Math.random()*9);
 			},
@@ -30,24 +35,42 @@ $(document).ready(function(){
 			// Highlights the random hole that was chosen from 
 			// getRandomHole function.
 			showMole: function(){
-				// console.log("show mole")
+				game.hideMole();
 				$(game.moles[game.getRandomHole()])
-					.addId("appear");
-					// THIS PART IS NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+					.fadeIn(500);
 			},
 
-			// designates what happens when user clicks.
-			userClickEvent: function(){
-
+			// Fades out all mole holes.
+			hideMole: function(){
+				game.moles.fadeOut(500);
 			},
-			// adds the score for the player
+
+			// Sets the interval to keep showing mole.
+			startInterval: function(){
+				game.interval = setInterval(game.showMole, 1000);
+			},
+
+			// Sets the timer interval to begin couting down.
+			startTimer: function(){
+				setInterval(game.updateTimer, 1000);
+			},
+				
+
+			// adds the score for the player.
 			addScore: function(){
-				this.score++;
-				// change html to reflect new score.
+				game.score++;
+				$("#score").html(game.score);
 			},
 
-			levelUp: function(){
-
+			// Determines whether the timer is completed or still counting.
+			updateTimer: function(){
+				if (game.timer == 0) {
+					alert("Your score was: " + game.score);
+					clearInterval(game.interval);
+				} else if (game.timer > 0) {
+					$("#timer").html(game.timer);
+				}
+				game.timer--;
 			}
 		};
 		game.init();
